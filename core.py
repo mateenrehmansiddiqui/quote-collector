40349e1ab991d2ff504f992361cf956891e28d9b
Mateen Siddiqui <siddiquimat@gmail.com>
2025-07-17 01:45:28 -0700

    Delete Quote Added

--- core.py
+++ core.py
@@ -9,3 +9,18 @@ def list_quotes(file_path = "quotes.txt"):
         quotes = file.readlines()
         for line in quotes:
             print("•", line.strip())
+
+def delete_quote(index, file_path = "quotes.txt"):
+    with open(file_path, "r") as file:
+        quotes = file.readlines()
+
+    if index >= 0 and index < len(quotes):
+        deleted_quote = quotes.pop(index)
+        with open(file_path, "w") as file:
+            file.writelines(quotes)
+
+        print(f"[x] Removed: {deleted_quote.strip()}")
+
+    else:
+        print("[!] Invalid Index.")
+
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from core import add_quote, list_quotes
+from core import add_quote, list_quotes, delete_quote
 
 print("----QUOTE-COLLECTOR----")
 
 add_quote("'The only way to do great work is to love what you do.' — Steve Jobs")
 add_quote("'Success is not final, failure is not fatal: It is the courage to continue that counts.' — Winston Churchill")
+
+list_quotes()
+
+delete_quote(1)
 
 list_quotes()
